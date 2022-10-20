f8db4fdb2147c4aa31d02aeaeeb606c6ac968678
Julian Steenbakker <jst@beverinnovations.com>
2022-10-20 09:35:40 +0200

    style: fix linter

--- lib/src/flutter_ble_central.dart
+++ lib/src/flutter_ble_central.dart
@@ -7,7 +7,6 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_ble_central/src/models/scan_result.dart';
